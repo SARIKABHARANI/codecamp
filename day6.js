f25ded00da9a9289b6b506a80b56230acefea467
SARIKABHARANI <sarikabharani@gmail.com>
2022-07-14 21:07:50 +0530

    project initiated

---
+++ day6.js
@@ -0,0 +1,11 @@
+console.log('connected')
+
+// function cookMaggi (maggi, water, pan){
+//     console.log ('Yours' + maggi + 'blocks of maggi', made in + water + 'liters of water in' + pan + 'pans is ready')
+// }
+// cookMaggi (2, 1, 2)
+
+// let result = cookMaggi (2, 2, 2)
+// console.log(result)
+
+
---
+++ day7.js
@@ -0,0 +1,22 @@
+console.log ('connected')
+function varlet (){
+    for (let i=o, i<5, i++){
+        console.log(i)
+    }
+    let i=6
+    console.log(i)
+}
+
+
+const btn = document.querySelector (#btn)
+const text = document.querySelector (#text)
+const body = document.getElementById (#body)
+const image = document.querySelector (#image)
+
+btn.addEventListener ('click', () => {
+    body.style.backgroundColor = 'blue'
+    body.stylecolor = 'black'
+    text.innerHTML = ''
+    text.innerHTML = '$(value)'
+    img.setAttriute
+})
